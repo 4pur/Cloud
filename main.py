f5b16ca2df9a078c87187bd66d9d8b9304949e58
rye <rvyeinq@gmail.com>
2023-04-20 15:19:32 +0000

    Content

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from os import getenv; TOKEN = getenv("TOKEN")
 
 class Cloud(discord.Client):
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f'online as cloud')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if message.content == "a":
+            print("a")
 
 client = Cloud(intents=discord.Intents.default())
 client.run(TOKEN)
