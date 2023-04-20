559f5ec81c28419094a9217450fee2180cb96543
rye <rvyeinq@gmail.com>
2023-04-20 15:56:48 +0000

    timeout & unban commands

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import datetime as dt
 from dotenv import load_dotenv; load_dotenv() # Read .env file contents
 from os import getenv; TOKEN = getenv("TOKEN")
 
@@ -14,11 +15,22 @@ async def ban(ctx, m: discord.Member, r = str):
 
 @commands.command()
 @commands.has_permissions(kick_members = True)
-async def kick(ctx, m: discord.Member, r = None):
+async def kick(ctx, m: discord.Member, r = str):
     for ms in m:
         await m.kick(reason = r)
         await ctx.send(f"Kicked {m} for {r}")
 
+@commands.command()
+@commands.has_permissions(moderate_members = True)
+async def timeout(ctx, m: discord.Member, r = str, u = str):
+    for ms in m:
+        await m.timeout(reason = r, until = dt.utcnow())
+
+@commands.command()
+@commands.has_permissions(ban_members = True)
+async def unban(ctx, m: discord.Member):
+    for ms in m:
+        await m.unban
 
 Cloud.add_command(ban)
 Cloud.add_command(kick)
