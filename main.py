2efc99cdca9a313144d422e1a629ebd3b37d0f97
rye <rvyeinq@gmail.com>
2023-04-21 19:16:03 +0000

    Gitacora saved 3 changed files

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from commands.moderation.unban   import UnbanCog
 from commands.moderation.kick    import KickCog
 from commands.moderation.timeout import TimeoutCog
 
-from commands.embed               import EmbedCog
+from commands.ai.ask             import AskCog
 
 Cloud = commands.Bot(command_prefix='$', intents=discord.Intents(message_content = True, members = True, messages = True))
 print("online!")
 
+#
 Cloud.add_cog(BanCog(Cloud))
 Cloud.add_cog(UnbanCog(Cloud))
 Cloud.add_cog(KickCog(Cloud))
 Cloud.add_cog(TimeoutCog(Cloud))
-Cloud.add_cog(EmbedCog(Cloud))
+
+# AI
+Cloud.add_cog(AskCog(Cloud))
 
 load_dotenv()
 TOKEN = getenv("TOKEN")
