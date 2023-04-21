537876ee6b781444ffa87ad5b2e8b15f8d58f59f
rye <rvyeinq@gmail.com>
2023-04-21 12:46:50 +0000

    Gitacora saved 1 changed files

--- commands/help.py
+++ commands/help.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 e = Embed(
     color=(0xeca4fb),
     title="Help")
-e.add_field("test")
+e.add_field(name="test")
 
 class HelpCog(commands.Cog):
     def __init__(self, bot):
