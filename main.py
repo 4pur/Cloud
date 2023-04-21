3f9cabc41cbfff44f450e67c64ea8ebf12d1df2d
rye <rvyeinq@gmail.com>
2023-04-20 22:05:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import discord
 
 from discord.ext import commands
-from dotenv import load_dotenv; load_dotenv() # Read .env file contents
-from os import getenv; TOKEN = getenv("TOKEN")
+from dotenv      import load_dotenv
+from os          import getenv
 
-from commands.moderation.ban import BanCog
-from commands.moderation.unban import UnbanCog
-from commands.moderation.kick import KickCog
+from commands.moderation.ban     import BanCog
+from commands.moderation.unban   import UnbanCog
+from commands.moderation.kick    import KickCog
 from commands.moderation.timeout import TimeoutCog
 
 Cloud = commands.Bot(command_prefix='.', intents=discord.Intents(message_content = True, members = True, messages = True))
@@ -16,4 +16,7 @@ Cloud.add_cog(BanCog(Cloud))
 Cloud.add_cog(UnbanCog(Cloud))
 Cloud.add_cog(KickCog(Cloud))
 Cloud.add_cog(TimeoutCog(Cloud))
+
+load_dotenv()
+TOKEN = getenv("TOKEN")
 Cloud.run(TOKEN)
