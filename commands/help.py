428433b31cc6582d50ee4c0a5c7fa2eacdb7ee40
rye <rvyeinq@gmail.com>
2023-04-21 14:16:50 +0000

    embed

--- commands/help.py
+++ commands/help.py
@@ -2,9 +2,8 @@ import discord
 from discord import Embed
 from discord.ext import commands
 
-e = Embed(
-    color=(0xeca4fb),
-    title="Help")
+e = Embed(color=(0xeca4fb), title="Help")
+
 e.add_field(name="test", 
             value="test2",
             inline=False)
