efd505ff8420d6e75129638fe4d5bde8e63e9ff8
rye <rvyeinq@gmail.com>
2023-04-21 12:23:36 +0000

    Gitacora saved 2 changed files

--- commands/help.py
+++ commands/help.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
-
 class HelpCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
-    async def help(ctx, m: discord.Member, r = str):
+    async def help(ctx, m: discord.Member):
         print("")
