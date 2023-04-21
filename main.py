fe997354c11b99f18f2037c44e971737971439ba
rye <114079592+rvye@users.noreply.github.com>
2023-04-20 21:28:28 -0500

    Gitacora saved 1 changed files

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
+from discord.ext import commands
 import discord
 import datetime as dt
-import sys
-
-from discord.ext import commands
 from dotenv import load_dotenv; load_dotenv() # Read .env file contents
-from os import getenv; TOKEN = getenv("TOKEN"); USERNAME = getenv("NAME")
-
-# Allows for importing moderation commands
-sys.path.append(f'C:\\Users\\xdcol\\Cloud\\commands\\moderation')
-import timeout
-import unban
-import kick
-import ban
+from os import getenv; TOKEN = getenv("TOKEN")
 
 Cloud = commands.Bot(command_prefix='.', intents=discord.Intents(message_content = True, members = True, messages = True))
 
+@commands.command()
 
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, m: discord.Member, r = str):
+    for ms in m:
+        await m.ban(reason = r)
+        await ctx.send(f"Banned {m} for {r}")
 
+@commands.command()
+@commands.has_permissions(kick_members = True)
+async def kick(ctx, m: discord.Member, r = str):
+    for ms in m:
+        await m.kick(reason = r)
+        await ctx.send(f"Kicked {m} for {r}")
 
+@commands.command()
+@commands.has_permissions(moderate_members = True)
+async def timeout(ctx, m: discord.Member, r = str, u = dt.utcnow()):
+    for ms in m:
+        await m.timeout(reason = r, until = u)
+        await ctx.send(f"Timed out {m} until {u}, for {r}.")
 
-
-
-
-
+@commands.command()
+@commands.has_permissions(ban_members = True)
+async def unban(ctx, m: discord.Member):
+    for ms in m:
+        await m.unban
+        ctx.send(f"Unbanned {u}.")
+        
+@commands.command()
+async def who(ctx, m: discord.Member):
+    if ctx.author.id == 293545401972424725:
+        ctx.send(f"You are {m}")
+    else:
+        ctx.send(f"I do not know you.")
 Cloud.add_command(timeout)
 Cloud.add_command(kick)
 Cloud.add_command(ban)
