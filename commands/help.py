fbc1c59ee9110fc5fb5be47ecd666ebabfde3eba
rye <rvyeinq@gmail.com>
2023-04-21 12:16:10 +0000

    Gitacora saved 1 changed files

--- commands/help.py
+++ commands/help.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class HelpCog(commands.Cog):
-        def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
