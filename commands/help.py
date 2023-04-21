5943a7397fa295ee0a300f5f3faee1d0cae4ba4b
rye <rvyeinq@gmail.com>
2023-04-21 14:19:00 +0000

    Gitacora saved 1 changed files

--- commands/help.py
+++ commands/help.py
@@ -13,5 +13,5 @@ class HelpCog(commands.Cog):
         self.bot = bot
         
     @commands.command(pass_context = True)
-    async def help(ctx):
+    async def help(ctx, c):
         await ctx.send(embed=e)
