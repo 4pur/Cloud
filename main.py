fd7b40f6f67caa7f5c50685a9bceb48e33382ddd
rye <rvyeinq@gmail.com>
2023-04-21 19:36:35 +0000

    Simplify imports & cog additions

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from discord.ext import commands
 from dotenv      import load_dotenv
 from os          import getenv
 
-from commands.moderation.ban     import BanCog
-from commands.moderation.unban   import UnbanCog
-from commands.moderation.kick    import KickCog
-from commands.moderation.timeout import TimeoutCog
-
-from commands.ai.ask             import AskCog
+for str in ["commands.moderation.ban", "commands.moderation.unban", "commands.moderation.kick", "commands.moderation.timeout", "commands.ai.ask"]:
+    exec(f"from {str} import {str.split('.')[-1].capitalize()}Cog")
+    print(f"imported {str}")
 
 Cloud = commands.Bot(command_prefix='$', intents=discord.Intents(message_content = True, members = True, messages = True))
 print("online!")
@@ -17,7 +14,6 @@ print("online!")
 # make a loop to do this for every cog imported
 for cog in [BanCog, UnbanCog, KickCog, TimeoutCog, AskCog]:
     Cloud.add_cog(cog(Cloud))
-
 
 load_dotenv()
 TOKEN = getenv("TOKEN")
