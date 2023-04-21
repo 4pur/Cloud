4ae1ae47f08a94b103920764502e419d5be80167
rye <114079592+rvye@users.noreply.github.com>
2023-04-20 21:51:09 -0500

    Gitacora saved 1 changed files

--- commands/moderation/ban.py
+++ commands/moderation/ban.py
@@ -6,7 +6,7 @@ class BanCog(commands.Cog):
         self.bot = bot
         
     @commands.command()
-    @commands.has_permissions(ban_members = True)
+    # @commands.has_permissions(ban_members = True) Causing unnessecary errors, fix later.
     async def ban(ctx, m: discord.Member, r = str):
         for ms in m:
             await m.ban(reason = r)
