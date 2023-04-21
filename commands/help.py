e7a7cebf7975515dcabcc660b26a716c300de492
rye <rvyeinq@gmail.com>
2023-04-21 12:16:07 +0000

    Gitacora saved 1 changed files

--- commands/help.py
+++ commands/help.py
@@ -2,4 +2,12 @@ import discord
 from discord.ext import commands
 
 class HelpCog(commands.Cog):
-    
+        def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    # @commands.has_permissions(moderate_members = True) Causing unnessecary errors, fix later.
+    async def timeout(ctx, m: discord.Member, r = str):
+        for ms in m:
+            await m.timeout(reason = r)
+            await ctx.send(f"Timed out {m} for {r}.")
