42773ab0e120475965a1d5a82c0fb2d1fdf72e77
rye <rvyeinq@gmail.com>
2023-04-21 12:50:21 +0000

    Gitacora saved 1 changed files

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import discord
 
+from add_commands import addCommands
 from discord.ext import commands
 from dotenv      import load_dotenv
 from os          import getenv
 
-
-
 Cloud = commands.Bot(command_prefix='.', intents=discord.Intents(message_content = True, members = True, messages = True))
 print("online!")
 
-
-
+addCommands()
 load_dotenv()
 TOKEN = getenv("TOKEN")
 Cloud.run(TOKEN)
