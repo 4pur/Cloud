6c85d813cc37acd3cf23297584dafbab2b07d39b
rye <114079592+rvye@users.noreply.github.com>
2023-04-20 21:47:00 -0500

    Gitacora saved 1 changed files

--- main.py
+++ main.py
@@ -11,32 +11,6 @@ from commands.moderation.timeout import TimeoutCog
 
 Cloud = commands.Bot(command_prefix='.', intents=discord.Intents(message_content = True, members = True, messages = True))
 print("online!")
-
-
-
-@commands.command()
-@commands.has_permissions(kick_members = True)
-async def kick(ctx, m: discord.Member, r = str):
-    for ms in m:
-        await m.kick(reason = r)
-        await ctx.send(f"Kicked {m} for {r}")
-
-
-
-@commands.command()
-@commands.has_permissions(ban_members = True)
-async def unban(ctx, m: discord.Member):
-    for ms in m:
-        await m.unban
-        ctx.send(f"Unbanned {u}.")
-        
-@commands.command()
-async def who(ctx):
-    if ctx.author.id == 293545401972424725:
-        await ctx.send(f"You are {ctx.author}")
-    else:
-        await ctx.send(f"I do not know you.")
-        
         
 Cloud.add_cog(BanCog(Cloud))
 Cloud.add_cog(UnbanCog(Cloud))
