08fb437ec8a6642ba06396c2768fd8848b3f2e15
rye <114079592+rvye@users.noreply.github.com>
2023-04-23 05:00:53 -0500

    i forgot

--- commands/misc/spam.py
+++ commands/misc/spam.py
@@ -1,22 +1,28 @@
 import discord
+import sys
 
 from discord.ext import commands
 from time import sleep
 
-x = False
+
+x = 0
 
 class SpamCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+        
+        
     @commands.command()
     async def startspam(self, ctx):
-        x = True
-        while x == True:
+        x = 1
+        
+        while x == 1:
             await ctx.send("z")
             sleep(1) # Avoid rate limits.
-    
+            
+                
     @commands.command()
     async def stopspam(self, ctx):
-        x = False
-        print("Stopped spamming...")
+        x = 0
+        await ctx.send("Stopped spamming.")
