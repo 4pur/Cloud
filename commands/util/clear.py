65c5f24f11c16fe2386775ac751dcf40d509584d
rye <rvyeinq@gmail.com>
2023-04-27 17:31:26 +0000

    add clear command

---
+++ commands/util/clear.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+from time import sleep
+
+class ClearCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(name="clear", aliases=["softclear", "clearmessages", "clearmsgs"])
+    async def Clear(self, ctx):
+        await ctx.send("clearing messages...")
+        await ctx.send("‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n")
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from os          import getenv
 
 i = 0
 
-for str in ["commands.moderation.ban", "commands.moderation.unban", "commands.moderation.kick", "commands.moderation.timeout", "commands.misc.spam", "commands.misc.avatar"]:
+for str in ["commands.moderation.ban", "commands.moderation.unban", "commands.moderation.kick", "commands.moderation.timeout", "commands.misc.spam", "commands.util.avatar", "commands.util.clear"]:
     i += 1
     exec(f"from {str} import {str.split('.')[-1].capitalize()}Cog")
     
@@ -19,7 +19,7 @@ intent.message_content = True
 
 Cloud = commands.Bot(command_prefix='$', intents=intent)
 
-for cog in [BanCog, UnbanCog, KickCog, TimeoutCog, SpamCog, AvatarCog]:
+for cog in [BanCog, UnbanCog, KickCog, TimeoutCog, SpamCog, AvatarCog, ClearCog]:
     Cloud.add_cog(cog(Cloud))
 
 logger = logging.getLogger('discord')
