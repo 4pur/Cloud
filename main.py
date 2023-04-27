5b0a51f9ec5701fbd6f8660628045e55ac188a22
rye <rvyeinq@gmail.com>
2023-04-27 17:14:00 +0000

    Add Logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import logging
 
 from discord.ext import commands
 from dotenv      import load_dotenv
@@ -17,6 +18,12 @@ print("online!")
 
 for cog in [BanCog, UnbanCog, KickCog, TimeoutCog, SpamCog]:
     Cloud.add_cog(cog(Cloud))
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv(); TOKEN = getenv("TOKEN")
 Cloud.run(TOKEN)
