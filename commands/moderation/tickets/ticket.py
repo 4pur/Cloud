c5b864a44b42c4d35e44a80bd61780314360c2e6
rye <rvyeinq@gmail.com>
2023-05-08 19:23:18 +0000

    hotfix

--- commands/moderation/tickets/ticket.py
+++ commands/moderation/tickets/ticket.py
@@ -31,4 +31,4 @@ class TicketEmbedView(discord.ui.View):
         channel = await interaction.guild.create_text_channel(name=f"ticket-{interaction.user}", category=self.id, reason=None)
         interaction.guild.get_channel(f"ticket-{interaction.user}").send(f"Ticket Created By {interaction.user.mention}")
         interaction.guild.get_channel(f"ticket-{interaction.user}").set_permissions(interaction.user, read_messages=True, send_messages=True)
-        await interaction.channel.send(f"ticket created... {channel.mention})
+        await interaction.channel.send(f"ticket created... {channel.mention}")
