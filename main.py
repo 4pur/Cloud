71ba3f981bc4dc907ef3411909011916545bfbd6
rye <rvyeinq@gmail.com>
2023-05-08 14:59:10 -0500

    Update main.py
    update imports

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ for str in ["commands.moderation.ban",
             "commands.moderation.warnings.warns",
             "commands.moderation.warnings.cw",
             "commands.moderation.tickets.ticket",
-            "commands.moderation.tickets.close"]:
+            "commands.moderation.tickets.close",
+            "commands.moderation.tickets.remove",
+            "commands.moderation.tickets.add"]:
     
     
     i += 1
@@ -60,7 +62,9 @@ for cog in [
     WarnsCog,
     CwCog,
     TicketCog,
-    CloseCog]:
+    CloseCog,
+    AddCog,
+    RemoveCog]:
     
     Cloud.add_cog(cog(Cloud))
 
