0bf82ffd56472bbc449d0b69ee5f5ae85863e809
rye <rvyeinq@gmail.com>
2023-05-08 18:17:37 +0000

    wip lol

--- commands/moderation/tickets/ticket.py
+++ commands/moderation/tickets/ticket.py
@@ -9,25 +9,14 @@ class TicketCog(commands.Cog):
     
     @commands.command(name="ticket", aliases=["t"])
     @commands.has_permissions(manage_messages=True)
-    async def ticket(self, ctx):
-        await ctx.send(view=TicketPromptView())
+    async def ticket(self, ctx, title, desc):
+        await ctx.send("creating embed...", view=TicketEmbedView(title = title, desc = desc))
         
-class TicketPromptView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.add_item(discord.ui.Button(label="Create Ticket Embed", custom_id="create_ticket_embed", style=discord.ButtonStyle.green))
-    
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_modal(CreateTicketPromptModal(title="Create Ticket Embed", description="Please enter the ticket categor(ies) and description."))
 
-class CreateTicketPromptModal(discord.ui.Modal):
-    def __init__(self, *args, **kwargs):
-        super().__init__(timeout=60.00, *args, **kwargs)
-        self.add_item(discord.ui.InputText(label="Ticket Category"))
-        self.add_item(discord.ui.InputText(label="Ticket Description", style = discord.InputTextStyle.long))
-        
-    async def callback(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Modal Results")
-        embed.add_field(name="Short Input", value=self.children[0].value)
-        embed.add_field(name="Long Input", value=self.children[1].value)
-        await interaction.response.send_message(embeds=[embed])
+class TicketEmbedView():
+    def __init__(self, title, desc, *args, **kwargs):
+        super.__init__(title, desc, *args, **kwargs)
+        this.title = title
+        this.desc = desc
+        e = discord.Embed(title=title, description=desc)
+        e.set_footer(text="")
--- commands/util/clear.py
+++ commands/util/clear.py
@@ -5,7 +5,7 @@ class ClearCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(name="clear", aliases=["softclear", "clearmessages", "clearmsgs"])
+    @commands.command(name="clear", aliases=["softclear", "clearmessages", "clearmsgs", "sc"])
     async def Clear(self, ctx):
         await ctx.send("clearing messages...")
         await ctx.send("‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n‎\n")
