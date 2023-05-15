87f4e1182a5aedae0fe67dd87f84ae583dfcbf0c
rye <rvyeinq@gmail.com>
2023-05-15 09:49:23 -0500

    Update lock.py

--- commands/moderation/lock.py
+++ commands/moderation/lock.py
@@ -5,8 +5,9 @@ from discord.ext import commands
 class LockCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+       
     @commands.command(name="lock", aliases=["lockchannel", "lockdown", "lockdownchannel", "lockc"])
+    @commands.has_permissions(manage_messages=True)
     async def lock(self, ctx):
         if ctx.channel.overwrites_for(ctx.guild.default_role).send_messages == False:
             await ctx.send("This channel is already locked.")
@@ -15,6 +16,7 @@ class LockCog(commands.Cog):
             await ctx.send("Channel locked.")
             
     @commands.command(name="unlock", aliases=["unlockchannel", "unlockc"])
+    @commands.has_permissions(manage_messages=True)
     async def unlock(self, ctx):
         if ctx.channel.overwrites_for(ctx.guild.default_role).send_messages == True:
             await ctx.send("This channel is not locked.")
