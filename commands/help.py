5ec1f616f8842f596aec52d2d85bae01b3f24180
rye <rvyeinq@gmail.com>
2023-05-15 10:26:41 -0500

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -40,28 +40,8 @@ class HelpCog(commands.Cog):
     async def help(self, ctx):
         global ctx1 
         ctx1 = ctx
-        await ctx.send(view=SelectView())
+        await ctx.send(view=ButtonView())
         
-class SelectView(discord.ui.View):
-    
-    @discord.ui.select(
-        placeholder = "Select a category",
-        min_values = 1,
-        max_values = 1,
-        options = [
-        discord.SelectOption(
-            label="Moderation",
-            description="Moderation commands."
-        )])
-    
-    async def callback(self, x, interaction: discord.Interaction):
-        e = discord.Embed(title="Help", description="Moderation Commands", color=0x00ff00, timestamp = datetime.datetime.utcnow())
-        
-        e.add_field(name="Ban", value=ban, inline=False)
-        e.add_field(name="Unban", value=unban, inline=False)
-        e.add_field(name="Kick", value=kick, inline=False)
-
-        await interaction.response.send_message(embed = e, view = ButtonView())
 
 class MiscView(discord.ui.View):
     def __init__(self, custom_id, label, *args, **kwargs):
