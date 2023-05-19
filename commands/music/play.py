2cae00a239895018bee727e7ebb1629db7191bdc
rye <114079592+rvye@users.noreply.github.com>
2023-05-18 19:11:08 -0500

    windows music !

--- commands/music/play.py
+++ commands/music/play.py
@@ -1,26 +1,41 @@
 import discord
 import os
-import youtube_dl
+import yt_dlp as youtube_dl
 
 from dotenv import load_dotenv
 from discord.ext import commands,tasks
 from commands.util.ytdl import YTDLSource
 
-discord.opus.load_opus('util/libopus.so.0.9.0')
+if os.name == 'nt':
+    ffmpeg = "util/nt/ffmpeg.exe"
+elif os.name == 'posix':
+    discord.opus.load_opus('util/posix/libopus.so.0.9.0')
+    ffmpeg = "util/posix/ffmpeg"
+    
 class PlayCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='play')
     async def play(self, ctx, *, url):
+        if self.bot.voice_clients == []:
+            if not ctx.message.author.voice:
+                await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+                return
+            else:
+                channel = ctx.message.author.voice.channel
+            await channel.connect()
+        else:
+            pass
+        
         server = ctx.message.guild
         voice_channel = server.voice_client
 
-        if not discord.opus.is_loaded():
-            raise RunTimeError('Opus failed to load')
+        #if not discord.opus.is_loaded():
+            #raise RuntimeError('Failed to load Opus!')
         async with ctx.typing():
             filename = await YTDLSource.from_url(url, loop=self.bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable="util/ffmpeg", source=filename))
+            voice_channel.play(discord.FFmpegPCMAudio(executable=ffmpeg, source=filename))
         await ctx.send('**Now playing:** {}'.format(filename))
 
     @commands.command(name='pause')
