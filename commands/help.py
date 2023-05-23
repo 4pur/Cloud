2a25afe823cca07d94fbba5c8e4736ab282e9b2e
rye <rvyeinq@gmail.com>
2023-05-23 17:54:13 +0000

    help

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,4 @@
 # being rewrote
-
 import discord
 
 from discord.ext import commands
@@ -24,31 +23,66 @@ class SelectView(discord.ui.Select):
         super().__init__(placeholder = "Select an option", max_values = 1, min_values = 1, options = options)
         
     async def callback(self, interaction: discord.Interaction):
-        
         if self.values[0] == "AI":
-            embed = discord.Embed(title="AI Commands", description="AI Commands", color=0x00ff00)
-            embed.add_field(name="AI", value="`talk` - Talk to me!\n`chat` - Chat with me!\n`ai` - AI commands")
-            await interaction.response.send_message(embed=embed, view=SelectView())
+            embed = discord.Embed(title="AI Commands")
+            embed.add_field(name="chat", value="chat with the bot")
+            await interaction.response.edit_message(embed=embed)
         elif self.values[0] == "Economy":
-            embed = discord.Embed(title="Economy Commands", description="Economy Commands", color=0x00ff00)
-            embed.add_field(name="Economy", value="`balance` - Check your balance!\n`beg` - Beg for money!\n`give` - Give someone money!\n`rob` - Rob someone!\n`work` - Work for money!\n`economy` - Economy commands")
-            await interaction.response.send_message(embed=embed, view=SelectView())
+            embed = discord.Embed(title="Economy Commands", description="Economy Commands")
+            embed.add_field(name="balance", value="check your balance")
+            embed.add_field(name="beg", value="beg for money")
+            embed.add_field(name="deposit", value="deposit money")
+            embed.add_field(name="give", value="give money to someone")
+            embed.add_field(name="rob", value="rob money from someone")
+            embed.add_field(name="shop", value="shop for items")
+            embed.add_field(name="withdraw", value="withdraw money")
+            await interaction.response.edit_message(embed=embed)
         elif self.values[0] == "Moderation":
-            embed = discord.Embed(title="Moderation Commands", description="Moderation Commands", color=0x00ff00)
-            embed.add_field(name="Moderation", value="`ban` - Ban someone!\n`kick` - Kick someone!\n`clear` - Clear messages!\n`mute` - Mute someone!\n`unmute` - Unmute someone!\n`moderation` - Moderation commands")
-            await interaction.response.send_message(embed=embed, view=SelectView())
+            embed = discord.Embed(title="Moderation Commands")
+            embed.add_field(name="ban", value="ban a member")
+            embed.add_field(name="kick", value="kick a member")
+            embed.add_field(name="mute", value="mute a member")
+            embed.add_field(name="unban", value="unban a member")
+            embed.add_field(name="unmute", value="unmute a member")
+            embed.add_field(name="warn", value="warn a member")
+            embed.add_field(name="warns", value="view a member's warnings")
+            embed.add_field(name="clearwarns", value="softclear messages")
+            embed.add_field(name="purge", value="delete a number of messages")
+            embed.add_field(name="slowmode", value="set the slowmode of a channel")
+            embed.add_field(name="lock", value="lock a channel")
+            embed.add_field(name="unlock", value="unlock a channel")
+            embed.add_field(name="nuke", value="clear ALL messages in a channel.")
+            embed.add_field(name="addrole", value="add a role to a member")
+            embed.add_field(name="removerole", value="remove a role from a member")
+            embed.add_field(name="setprefix", value="set the server's prefix")
+            embed.add_field(name="autorole", value="set a role to be given to new members")
+            embed.add_field(name = "ticket", value = "create a ticket message")
+            embed.add_field(name = "close", value = "close a ticket")
+            embed.add_field(name = "add", value = "add a member to a ticket")
+            embed.add_field(name = "remove", value = "remove a member from a ticket")
+            await interaction.response.edit_message(embed=embed)
         elif self.values[0] == "Music":
-            embed = discord.Embed(title="Music Commands", description="Music Commands", color=0x00ff00)
-            embed.add_field(name="Music", value="`join` - Join a voice channel!\n`leave` - Leave a voice channel!\n`play` - Play a song!\n`pause` - Pause a song!\n`resume` - Resume a song!\n`stop` - Stop a song!\n`queue` - See the queue!\n`skip` - Skip a song!\n`music` - Music commands")
-            await interaction.response.send_message(embed=embed, view=SelectView())
+            embed = discord.Embed(title="Music Commands")
+            embed.add_field(name="join", value="join a voice channel")
+            embed.add_field(name="leave", value="leave a voice channel")
+            embed.add_field(name="pause", value="pause music")
+            embed.add_field(name="play", value="play music")
+            embed.add_field(name="queue", value="view the queue")
+            embed.add_field(name="remove", value="remove a song from the queue")
+            embed.add_field(name="resume", value="resume music")
+            embed.add_field(name="skip", value="skip a song")
+            embed.add_field(name="stop", value="stop music")
+            await interaction.response.edit_message(embed=embed)
         elif self.values[0] == "Utility":
-            embed = discord.Embed(title="Utility Commands", description="Utility Commands", color=0x00ff00)
-            embed.add_field(name="Utility", value="`ping` - Check my ping!\n`help` - See this message!\n`invite` - Invite me!\n`utility` - Utility commands")
-            await interaction.response.send_message(embed=embed, view=SelectView())
-
+            embed = discord.Embed(title="Utility Commands")
+            embed.add_field(name="avatar", value="view someone's avatar")
+            embed.add_field(name="help", value="view the help menu")
+            embed.add_field(name="invite", value="invite the bot to your server")
+            embed.add_field(name="ping", value="view the bot's latency")
+            embed.add_field(name="whois", value="view a user's information")
+            await interaction.response.edit_message(embed=embed)
+            
 class View(discord.ui.View):
     def __init__(self):
-        
         super().__init__()
         self.add_item(SelectView())
-        
